cfe51e2d7f7bb60c75fadfb0c1acf42c4765562a
Steve Baker <steve_m_baker@hotmail.com>
2019-06-19 15:13:38 +0100

    Changed .tf parameters

--- terraform/nginxdemocluster.tf
+++ terraform/nginxdemocluster.tf
@@ -1,26 +1,18 @@
-resource "azurerm_resource_group" "test" {
+resource "steevaavoo_resource_group" "test" {
   name     = "acctestRG1"
   location = "East US"
 }
 
-resource "azurerm_kubernetes_cluster" "test" {
+resource "steevaavoo_kubernetes_cluster" "test" {
   name                = "acctestaks1"
-  location            = "${azurerm_resource_group.test.location}"
-  resource_group_name = "${azurerm_resource_group.test.name}"
+  location            = "${steevaavoo_resource_group.test.location}"
+  resource_group_name = "${steevaavoo_resource_group.test.name}"
   dns_prefix          = "acctestagent1"
 
   agent_pool_profile {
     name            = "default"
-    count           = 1
+    count           = 2
     vm_size         = "Standard_D1_v2"
-    os_type         = "Linux"
-    os_disk_size_gb = 30
-  }
-
-  agent_pool_profile {
-    name            = "pool2"
-    count           = 1
-    vm_size         = "Standard_D2_v2"
     os_type         = "Linux"
     os_disk_size_gb = 30
   }
@@ -36,9 +28,9 @@ resource "azurerm_kubernetes_cluster" "test" {
 }
 
 output "client_certificate" {
-  value = "${azurerm_kubernetes_cluster.test.kube_config.0.client_certificate}"
+  value = "${steevaavoo_kubernetes_cluster.test.kube_config.0.client_certificate}"
 }
 
 output "kube_config" {
-  value = "${azurerm_kubernetes_cluster.test.kube_config_raw}"
+  value = "${steevaavoo_kubernetes_cluster.test.kube_config_raw}"
 }
