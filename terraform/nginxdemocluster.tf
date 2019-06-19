d0e17d4dbc8eb15fbd9374afa61987234c38236e
Steve Baker <steve_m_baker@hotmail.com>
2019-06-19 15:23:10 +0100

    Changed .tf parameters

--- terraform/nginxdemocluster.tf
+++ terraform/nginxdemocluster.tf
@@ -1,30 +1,21 @@
-resource "azurerm_resource_group" "test" {
-  name     = "acctestRG1"
+resource "azurerm_resource_group" "steevaavoo" {
+  name     = "steevaavooRG1"
   location = "East US"
 }
 
-resource "azurerm_kubernetes_cluster" "test" {
-  name                = "acctestaks1"
-  location            = "${azurerm_resource_group.test.location}"
-  resource_group_name = "${azurerm_resource_group.test.name}"
-  dns_prefix          = "acctestagent1"
+resource "azurerm_kubernetes_cluster" "steevaavoo" {
+  name                = "steevaavooaks1"
+  location            = "${azurerm_resource_group.steevaavoo.location}"
+  resource_group_name = "${azurerm_resource_group.steevaavoo.name}"
+  dns_prefix          = "steevaavooagent1"
 
   agent_pool_profile {
     name            = "default"
-    count           = 1
+    count           = 2
     vm_size         = "Standard_D1_v2"
     os_type         = "Linux"
     os_disk_size_gb = 30
   }
-
-  agent_pool_profile {
-    name            = "pool2"
-    count           = 1
-    vm_size         = "Standard_D2_v2"
-    os_type         = "Linux"
-    os_disk_size_gb = 30
-  }
-
   service_principal {
     client_id     = "__clientid__"
     client_secret = "__clientsecret__"
@@ -36,9 +27,9 @@ resource "azurerm_kubernetes_cluster" "test" {
 }
 
 output "client_certificate" {
-  value = "${azurerm_kubernetes_cluster.test.kube_config.0.client_certificate}"
+  value = "${azurerm_kubernetes_cluster.steevaavoo.kube_config.0.client_certificate}"
 }
 
 output "kube_config" {
-  value = "${azurerm_kubernetes_cluster.test.kube_config_raw}"
+  value = "${azurerm_kubernetes_cluster.steevaavoo.kube_config_raw}"
 }
