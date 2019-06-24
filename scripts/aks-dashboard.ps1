df16d7e6ef1fc07c2b368edfc981900cbc6bc680
Steve Baker <steve_m_baker@hotmail.com>
2019-06-24 11:32:15 +0100

    Added config instruction

--- scripts/aks-dashboard.ps1
+++ scripts/aks-dashboard.ps1
@@ -1,6 +1,7 @@
 # Get the access credentials for the Kubernetes cluster
 # Creds are merged into your current console session, eg:
-# Merged "steevaavoo-aks-cluster01" as current context in /home/adam/.kube/config
+# Merged "steevaavoo-aks-cluster01" as current context in /home/steve/.kube/config
+# You'll need to delete "config" if you have previously done this
 az aks get-credentials --resource-group stvRG1 --name stvaks1
 
 # Show k8s nodes / pods
