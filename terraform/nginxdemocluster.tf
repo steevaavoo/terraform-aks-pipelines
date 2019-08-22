c1e2669e7491145c301eb96fc788f8748943594e
Steve Baker <steve_m_baker@hotmail.com>
2019-08-22 08:29:36 +0100

    Changed all variables to reside in variables.tf

--- terraform/nginxdemocluster.tf
+++ terraform/nginxdemocluster.tf
@@ -2,10 +2,10 @@
 terraform {
   required_version = ">= 0.11"
   backend "azurerm" {
-    storage_account_name = "__terraformstorageaccount__"
-    container_name       = "terraform"
-    key                  = "terraform.tfstate"
-    access_key           = "__storagekey__"
+    storage_account_name = "${var.backend_storage_account_name}"
+    container_name       = "${var.backend_storage_container_name}"
+    key                  = "${var.backend_storage_key}"
+    access_key           = "${var.backend_storage_access_key}"
   }
 }
 resource "azurerm_resource_group" "stvrg" {
@@ -16,26 +16,26 @@ resource "azurerm_container_registry" "stvacr" {
   name                = "${var.container_registry_name}"
   resource_group_name = "${azurerm_resource_group.stvrg.name}"
   location            = "${azurerm_resource_group.stvrg.location}"
-  admin_enabled       = false
-  sku                 = "Basic"
+  admin_enabled       = "${var.acr_admin_enabled}"
+  sku                 = "${var.acr_sku}"
 }
 
 resource "azurerm_kubernetes_cluster" "stvaks" {
-  name                = "__aksclustername__"
+  name                = "${var.azurerm_kubernetes_cluster_name}"
   location            = "${azurerm_resource_group.stvrg.location}"
   resource_group_name = "${azurerm_resource_group.stvrg.name}"
   dns_prefix          = "${var.aks_dns_prefix}"
 
   agent_pool_profile {
-    name            = "default"
+    name            = "${var.agent_pool_profile_name}"
     count           = "${var.agent_pool_count}"
-    vm_size         = "Standard_D1_v2"
-    os_type         = "Linux"
-    os_disk_size_gb = 30
+    vm_size         = "${var.agent_pool_profile_vm_size}"
+    os_type         = "${var.agent_pool_profile_os_type}"
+    os_disk_size_gb = "${var.agent_pool_profile_disk_size_gb}"
   }
   service_principal {
-    client_id     = "__clientid__"
-    client_secret = "__clientsecret__"
+    client_id     = "${var.service_principal_client_id}"
+    client_secret = "${var.service_principal_client_secret}"
   }
 
   tags = {
--- terraform/variables.tf
+++ terraform/variables.tf
@@ -1,6 +1,31 @@
+variable "backend_storage_account_name" {
+  default = "__terraformstorageaccount__"
+}
+
+variable "backend_storage_container_name" {
+  default = "terraform"
+}
+
+variable "backend_storage_key" {
+  default = "terraform.tfstate"
+}
+
+variable "backend_storage_access_key" {
+  default = "__storagekey__"
+}
+
 variable "container_registry_name" {
   default = "stvcontReg1"
 }
+
+variable "acr_admin_enabled" {
+  default = false
+}
+
+variable "acr_sku" {
+  default = "Basic"
+}
+
 
 variable "aks_dns_prefix" {
   default = "stvagent1"
@@ -17,3 +42,32 @@ variable "location" {
 variable "agent_pool_count" {
   default = 2
 }
+
+variable "azurerm_kubernetes_cluster_name" {
+  default = "__aksclustername__"
+}
+
+variable "agent_pool_profile_name" {
+  default = "default"
+}
+
+variable "agent_pool_profile_vm_size" {
+  default = "Standard_D1_v2"
+}
+
+variable "agent_pool_profile_os_type" {
+  default = "Linux"
+}
+
+variable "agent_pool_profile_disk_size_gb" {
+  default = 30
+}
+
+variable "service_principal_client_id" {
+  default = "__clientid__"
+}
+
+variable "service_principal_client_secret" {
+  default = "__clientsecret__"
+}
+
