74792387ebd39b59bb140b306df42bf40b0a3b86
Steve Baker <steve_m_baker@hotmail.com>
2019-08-24 12:35:44 +0100

    Changed inline code to script files Changed build agent for Terraform

---
+++ scripts/Get-StorageKey.ps1
@@ -0,0 +1,3 @@
+$key = (Get-AzStorageAccountKey -ResourceGroupName $(terraformstoragerg) -AccountName $(terraformstorageaccount)).Value[0]
+
+Write-Host "##vso[task.setvariable variable=storagekey]$key"
