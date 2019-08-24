edcf6294b4211220dcfe632f7b5d637f1866af19
Steve Baker <steve_m_baker@hotmail.com>
2019-08-24 13:08:50 +0100

    Changed environment vars

--- scripts/Get-StorageKey.ps1
+++ scripts/Get-StorageKey.ps1
@@ -1,3 +1,3 @@
-$key = (Get-AzStorageAccountKey -ResourceGroupName $(terraformstoragerg) -AccountName $(terraformstorageaccount)).Value[0]
+$key = (Get-AzStorageAccountKey -ResourceGroupName $env:TERRAFORMSTORAGERG -AccountName $env:TERRAFORMSTORAGEACCOUNT).Value[0]
 
 Write-Host "##vso[task.setvariable variable=storagekey]$key"
