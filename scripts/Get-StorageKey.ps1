5400ba159bd9493aa5f33ad49e1b7e0f829ccab3
Steve Baker <steve_m_baker@hotmail.com>
2019-08-24 14:10:33 +0100

    Added key check

--- scripts/Get-StorageKey.ps1
+++ scripts/Get-StorageKey.ps1
@@ -1,3 +1,4 @@
 $key = (Get-AzStorageAccountKey -ResourceGroupName $env:TERRAFORMSTORAGERG -AccountName $env:TERRAFORMSTORAGEACCOUNT).Value[0]
 
 Write-Host "##vso[task.setvariable variable=storagekey]$key"
+Write-Host $key -ForegroundColor Yellow -BackgroundColor Blue
