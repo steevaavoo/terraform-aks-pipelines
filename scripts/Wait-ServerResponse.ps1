66bf107f16e0d987b29e574dfd274ece4d339cc5
Steve Baker <steve_m_baker@hotmail.com>
2019-08-31 11:15:36 +0100

    Added timeout counter

--- scripts/Wait-ServerResponse.ps1
+++ scripts/Wait-ServerResponse.ps1
@@ -17,12 +17,22 @@ param (
     $DomainName,
     $HeaderString,
     $RequestTimeoutSec,
-    $TaskTimeoutSec
+    $TaskTimeoutSec,
+    $RetryIntervalSec = 5
 )
 
-$response = $null
+$timer = [Diagnostics.Stopwatch]::StartNew()
+
 while (-not ($response.Headers.Server -match $HeaderString)) {
-    Write-Output "Waiting for value [$HeaderString] in Response Headers Server..."
-    $response = Invoke-WebRequest -Method Get -Uri $DomainName -TimeoutSec $RequestTimeoutSec
+
+    if ($timer.Elapsed.TotalSeconds -gt $TaskTimeoutSec) {
+        Write-Output "##vso[task.logissue type=error]Elapsed task time of [$($timer.Elapsed.TotalSeconds)] has exceeded timeout of [$TimeoutSeconds]"
+        exit 1
+    } else {
+        Write-Output "Waiting for value [$HeaderString] in Response Headers Server..."
+        $response = Invoke-WebRequest -Method Get -Uri $DomainName -TimeoutSec $RequestTimeoutSec
+        Start-Sleep -Seconds $RetryIntervalSec
+    }
 }
+
 Write-Output "Found value [$HeaderString] in Response Headers Server - test succeeded"
